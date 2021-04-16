abb304affcdac781c6ce3d3b4c5600f0b7120777
ozzychel <andrey.petunin86@gmail.com>
2021-04-15 17:04:08 -0700

    climbingLeaderBoard

--- climbingLeaderBoard/climbingLeaderBoard.js
+++ climbingLeaderBoard/climbingLeaderBoard.js
@@ -3,43 +3,31 @@ An arcade game player wants to climb to the top of the leaderboard and track the
 
 -The player with the highest score is ranked number 1  on the leaderboard.
 -Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately following ranking number.
-Complete the climbingLeaderboard function to return array of players rank after each new score
+Complete the climbingLeaderboard function to return ay of players rank after each new score
 */
 
-function climbingLeaderboard(scores, alice) {
-  let result = [];
-  let uniqueScores = [...new Set(scores)];
-  for (const score of alice) {
-    if (score >= uniqueScores[0]) {
-      result.push(1);
-    } else if (score < uniqueScores[uniqueScores.length - 1]) {
-      result.push(uniqueScores.length + 1);
-    } else {
-      result.push(rankBinarySearch(score, uniqueScores));
-    }
-  }
-  return result;
-}
-function rankBinarySearch(score, uniqueScores) {
-  let start = 0;
-  let end = uniqueScores.length - 1;
-  while (true) {
-    let mid = Math.floor((start + end) / 2);
-    if (uniqueScores[mid] === score) {
-      return mid + 1;
-    } else if (uniqueScores[mid] > score && uniqueScores[mid + 1] < score) {
-      return mid + 2;
-    } else if (uniqueScores[mid] < score && uniqueScores[mid - 1 > score]) {
-      return mid - 1;
-    }
-    if (score < uniqueScores[mid]) {
-      start = mid + 1;
-    } else {
-      end = mid - 1;
-    }
-  }
+function climbingLeaderboard (scores, alice) {
+   let results = [];
+   let unique = [...new Set(scores)];
+   for(let score of alice) {
+     if(score > unique[0]) results.push(1);
+     else if (score < unique[unique.length - 1]) results.push(unique.length + 1);
+     else results.push(rankBinarySearch(score, unique))
+   }
+   return results;
 }
 
+function rankBinarySearch (score, nums) {
+  let left = 0, mid = 0, right = nums.length - 1;
+  while (true) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === score) return mid + 1;
+    else if (nums[mid] > score && nums[mid + 1] < score) return mid + 2;
+    else if (nums[mid] < score && nums[mid - 1 > score]) return mid - 1;
+    if (score < nums[mid]) left = mid + 1;
+    else right = mid - 1;
+  }
+}
 
 console.log(climbingLeaderboard([10], [15])) // [1]
 console.log(climbingLeaderboard([10], [5])) // [2]
